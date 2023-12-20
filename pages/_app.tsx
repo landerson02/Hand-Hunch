5e48a4229ee649ea8e8ce53595ad2f0a77d491d9
Lucas Anderson <lucas.a@comcast.net>
2023-12-19 18:43:18 -0600

    Add analytics tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Noto_Sans_Display } from 'next/font/google'
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react";
 
 const font = Noto_Sans_Display ({
   subsets: ['latin'],
@@ -10,6 +12,8 @@ const font = Noto_Sans_Display ({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={`${font.className} font-semibold`}>
+      <SpeedInsights />
+      <Analytics />
       <Component {...pageProps} />
     </main>
   )
