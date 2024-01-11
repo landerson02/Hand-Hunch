63a9981540a01cc2ada9effbedfe0f47431a8c6b
Lucas Anderson <lucas.a@comcast.net>
2024-01-10 23:26:43 -0600

    Add password encryption and jwt authorization

--- pages/api/addUser.ts
+++ pages/api/addUser.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import clientPromise from '@/lib/mongodb'
 import { StatsObject} from "@/objects/stats";
+import bcrypt from 'bcryptjs';
 
 interface User {
   username: string;
@@ -10,13 +11,13 @@ interface User {
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { username, password , userStats} = req.body as User;
-
+  const hashedPassword = await bcrypt.hash(password, 10);
   try {
     const client = await clientPromise;
     const db = client.db('HandHunch');
     const usersCollection = db.collection('users');
 
-    const newUser = await usersCollection.insertOne({ username, password, userStats });
+    const newUser = await usersCollection.insertOne({ username, password: hashedPassword, userStats });
     if (newUser.insertedId) {
       const insertedUser = await usersCollection.findOne({ _id: newUser.insertedId });
       res.status(201).json({ message: 'User added!', user: insertedUser });
--- pages/api/signIn.ts
+++ pages/api/signIn.ts
@@ -1,5 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import clientPromise from '@/lib/mongodb';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { username, password } = req.body;
@@ -14,13 +16,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       return res.status(200).json({ message: 'Username not found' });
     }
 
-    // TODO - add bcrypt/jwt stuff
-    const isCorrectPwd= password === user.password;
+    const isCorrectPwd= await bcrypt.compare(password, user.password);
     if(!isCorrectPwd) {
       return res.status(200).json({ message: 'Incorrect password' });
     }
-
-    return res.status(200).json({ message: 'Logged in successfully', user: user});
+    if(!process.env.JWT_SECRET) {
+      console.error('JWT_SECRET missing from .env file');
+      return res.status(500).json({ error: 'Error logging in user' });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d'});
+    return res.status(200).json({ message: 'Logged in successfully', user: user, token});
   } catch (e) {
     console.error('Error logging in user:', e);
     res.status(500).json({ error: 'Error logging in user' });
